e6697891034352b1979e25a1e0056bbda1f96f77
ankushrawat123 <ankushrwt344@gmail.com>
2022-04-22 11:13:18 +0530

    Add project files.

---
+++ RFP_Day6_ASSIGNMENT/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RFP_Day6_ASSIGNMENT;
+Console.WriteLine("\n Welcome to Program for Fibonacci ................................");
+Console.WriteLine("Enter any number till that you want Fibonacci series");
+int Input = Convert.ToInt32(Console.ReadLine());
+Fibonacci obj1 = new Fibonacci();
+obj1.fibonacciMethod(Input);
+       
+
+
